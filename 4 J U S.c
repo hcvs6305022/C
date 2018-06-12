38f899ceea910a388c41eb0786b03ad6fe6798e2
hcvs6305022 <40188732+hcvs6305022@users.noreply.github.com>
2018-06-12 10:40:31 +0800

    Update 4 J U S.c

--- 4 J U S.c
+++ 4 J U S.c
@@ -27,3 +27,33 @@ int main() {
 	}	
 	return 0;
 }
+\\完成
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int a,b;
+	char c;
+	printf("輸入數值1,2及運算值:");	
+	scanf("%d %d %c",&a,&b,&c);
+
+	switch(c){
+		case'+':
+			printf("\n答案:%d",a+b);
+			break;
+		case'-':
+			printf("\n答案:%d",a-b);
+			break;
+		case'*':
+			printf("\n答案:%d",a*b);
+			break;	
+		case'/':
+			printf("\n答案:%d",a/b);
+			break;
+		default:
+		printf("輸入錯誤");
+			break;
+	}	
+	return 0;
+}
